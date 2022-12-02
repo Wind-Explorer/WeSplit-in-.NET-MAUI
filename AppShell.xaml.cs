8c111df2fc9666c9eda18b4370370c00c3b989da
Adam C <66894537+Wind-Explorer@users.noreply.github.com>
2022-12-02 22:24:34 +0800

    Add project files.

---
+++ AppShell.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace WeSplit;
+
+public partial class AppShell : Shell
+{
+	public AppShell()
+	{
+		InitializeComponent();
+    }
+}
---
+++ MainPage.xaml.cs
@@ -0,0 +1,21 @@
+﻿namespace WeSplit;
+
+public partial class MainPage : ContentPage
+{
+
+	public MainPage()
+	{
+		InitializeComponent();
+        InitialisePeopleCounter();
+	}
+
+    public void InitialisePeopleCounter()
+    {
+        for (var c = 2; c <= 100; c++)
+        {
+            PeopleCountPicker.Items.Add($"{c.ToString()} people");
+        }
+        PeopleCountPicker.SelectedIndex = 2;
+    }
+}
+
