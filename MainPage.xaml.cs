ddb565059e5af2667aa90339b5e9d889b8635e2c
Adam C <66894537+Wind-Explorer@users.noreply.github.com>
2022-12-03 13:10:33 +0800

    Layed out UI

--- MainPage.xaml.cs
+++ MainPage.xaml.cs
@@ -6,16 +6,28 @@ public partial class MainPage : ContentPage
 	public MainPage()
 	{
 		InitializeComponent();
-        InitialisePeopleCounter();
+        InitializePeopleCounter();
+        InitializeTipPercentageSlider();
 	}
 
-    public void InitialisePeopleCounter()
+    public void InitializePeopleCounter()
     {
         for (var c = 2; c <= 100; c++)
         {
             PeopleCountPicker.Items.Add($"{c.ToString()} people");
         }
         PeopleCountPicker.SelectedIndex = 2;
+    }
+
+    public void InitializeTipPercentageSlider()
+    {
+        TipPercentageSlider.Value = 10;
+        TipPercentageText.Text = $"{((int)TipPercentageSlider.Value).ToString()}%";
+    }
+
+    private void TipPercentageSlider_ValueChanged(object sender, ValueChangedEventArgs e)
+    {
+        TipPercentageText.Text = $"{((int)TipPercentageSlider.Value).ToString()}%";
     }
 }
 
