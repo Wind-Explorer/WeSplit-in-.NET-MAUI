34a99502cd47522b593dac9dfc5f4a6fb5233ee2
Adam C <66894537+Wind-Explorer@users.noreply.github.com>
2022-12-03 19:05:05 +0800

    Set up basic functions

--- MainPage.xaml.cs
+++ MainPage.xaml.cs
@@ -2,13 +2,17 @@
 
 public partial class MainPage : ContentPage
 {
+    private bool finishedInitializing = false;
 
-	public MainPage()
-	{
-		InitializeComponent();
+    public MainPage()
+    {
+        InitializeComponent();
         InitializePeopleCounter();
         InitializeTipPercentageSlider();
-	}
+        InitializeCostTextBox();
+        finishedInitializing= true;
+        CalculateCost();
+    }
 
     public void InitializePeopleCounter()
     {
@@ -25,9 +29,38 @@ public partial class MainPage : ContentPage
         TipPercentageText.Text = $"{((int)TipPercentageSlider.Value).ToString()}%";
     }
 
+    public void InitializeCostTextBox()
+    {
+        CostTextBox.Text = $"80.00";
+    }
+
     private void TipPercentageSlider_ValueChanged(object sender, ValueChangedEventArgs e)
     {
         TipPercentageText.Text = $"{((int)TipPercentageSlider.Value).ToString()}%";
+        CalculateCost();
+    }
+
+    private void CalculateCost()
+    {
+        if (finishedInitializing)
+        {
+            var str = new string((from c in CostTextBox.Text where char.IsNumber(c) || $"{c}" == "." select c).ToArray());
+            CostTextBox.Text = str;
+            if (str.Length > 0)
+            {
+                AmountPayablePerPerson.Text = $"${Math.Round(double.Parse(str) / 100 * (100 + (int)TipPercentageSlider.Value) / (PeopleCountPicker.SelectedIndex + 2), 2)}";
+            }
+        }
+    }
+
+    private void CostTextBox_TextChanged(object sender, TextChangedEventArgs e)
+    {
+        CalculateCost();
+    }
+
+    private void PeopleCountPicker_SelectedIndexChanged(object sender, EventArgs e)
+    {
+        CalculateCost();
     }
 }
 
